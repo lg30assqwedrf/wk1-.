62e1bef756a7ef219e28ad0a3d756e15a6c7a69c
barry <barry0326@gmail.com>
2020-03-09 22:25:10 +0800

    Hello Rn

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { getPlatformOrientationLockAsync } from 'expo/build/ScreenOrientation/ScreenOrientation';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>110719009 劉博宇</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
